1a2bed42453cb4b08448ad56ee6d87d4a1f1b8d8
Aleksei Mialkin <anmyalkin@gmail.com>
2023-05-29 19:35:41 +0300

    Add Create action

--- src/Dictionary.Api/Words/WordsController.cs
+++ src/Dictionary.Api/Words/WordsController.cs
@@ -13,15 +13,21 @@ public class WordsController : ControllerBase
         return Ok("List of words " + DateTime.Now);
     }
 
+    [HttpPost("create")]
+    public IActionResult Create()
+    {
+        return Ok("Word created " + DateTime.Now);
+    }
+
+    [HttpPut("update")]
+    public IActionResult Update()
+    {
+        return Ok("Word updated " + DateTime.Now);
+    }
+
     [HttpDelete("delete")]
     public IActionResult Delete()
     {
         return Ok("Word deleted " + DateTime.Now);
-    }
-
-    [HttpDelete("update")]
-    public IActionResult Update()
-    {
-        return Ok("Word updated " + DateTime.Now);
     }
 }
