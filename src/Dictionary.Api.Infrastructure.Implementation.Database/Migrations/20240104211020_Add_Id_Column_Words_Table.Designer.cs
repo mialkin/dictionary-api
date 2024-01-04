7b1de777fbb9e8695f96510005ff0c80907321d8
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-05 00:14:06 +0300

    Add ID column

--- src/Dictionary.Api.Infrastructure.Implementation.Database/Migrations/20240104211020_Add_Id_Column_Words_Table.Designer.cs
+++ src/Dictionary.Api.Infrastructure.Implementation.Database/Migrations/20240104211020_Add_Id_Column_Words_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Dictionary.Api.Infrastructure.Implementation.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Dictionary.Api.Infrastructure.Implementation.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240104211020_Add_Id_Column_Words_Table")]
+    partial class Add_Id_Column_Words_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Dictionary.Api.Infrastructure.Implementation.Database/Migrations/20240104211020_Add_Id_Column_Words_Table.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dictionary.Api.Infrastructure.Implementation.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Id_Column_Words_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                table: "words",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "words");
+        }
+    }
+}
