65fd45abbeb662fbd21e7984e98fdfce9bafcb4d
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-11 22:02:49 +0300

    Revert "run dotnet format"
    This reverts commit 05287adc843cec472a214b621e326846b666a6f4.

--- src/Dictionary.Api.UseCases/Words/Queries/SearchWords/SearchWordsQueryHandler.cs
+++ src/Dictionary.Api.UseCases/Words/Queries/SearchWords/SearchWordsQueryHandler.cs
@@ -37,7 +37,7 @@ internal class SearchWordsQueryHandler(IReadOnlyDatabaseContext readOnlyDatabase
                 x.UpdatedAt)
             )
             .Take(50) // TODO Move to specification and reuse across different methods?
-                      // TODO Implement client pagination with default value set on the client and on the server
+            // TODO Implement client pagination with default value set on the client and on the server
             .ToListAsync(cancellationToken);
 
         return words;
