9d6d33cd235101f22955a086f9e718341fbf26fa
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-24 16:05:48 +0300

    Add Add_Gender_Columns migration

---
+++ src/Dictionary.Api.Infrastructure.Implementation.Database/Migrations/20240324125527_Add_Gender_Columns.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dictionary.Api.Infrastructure.Implementation.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Gender_Columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "gender_feminine",
+                table: "words",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "gender_masculine",
+                table: "words",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "gender_neuter",
+                table: "words",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gender_feminine",
+                table: "words");
+
+            migrationBuilder.DropColumn(
+                name: "gender_masculine",
+                table: "words");
+
+            migrationBuilder.DropColumn(
+                name: "gender_neuter",
+                table: "words");
+        }
+    }
+}
